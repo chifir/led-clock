50c58303d72e33fd2f3249d30ad86ce6159501a3
4ifir <4ifirus@gmail.com>
2023-11-27 01:31:23 +0300

    Display current time in hh:mm

--- src/main.cpp
+++ src/main.cpp
@@ -4,73 +4,26 @@
 #include <GyverMAX7219.h>
 
 #define DEBUG true
-#define CLOCK_INTERRUPT_PIN 4
+#define CLOCK_INTERRUPT_PIN 2
 #define MODE_BUTTON_PIN 6
 
-enum dispalay_mode{
-  DECIMAL = 0,
-  BINARY = 1,
-  OCTAL = 2,
-  TEXT = 3
-};
+const uint64_t DELTA = 410455800;
 
-// target date
-TimeSpan target_date = TimeSpan(10000);
+volatile uint32_t seconds = 0;
 
-// display mode by default
-int8_t DISPLAY_MODE = dispalay_mode::DECIMAL;
-// SDA on D3, SCL on D2, SQW on D4
+// SDA on A4, SCL on A5, SQW on D4
 RTC_DS3231 rtc; 
-// 12 matrix in 1 row on D5
-MAX7219 <12 , 1, 5> mtrx; 
+RTC_I2C rtc_i2c;
 
-DateTime time;
-
-
-void display_string(const char* msg) {
-  mtrx.println(msg);
-  mtrx.update();
-}
-
-void display_decimal(uint32_t decmal){
-  mtrx.println(decmal);
-  mtrx.update();
-}
-
-
+// 4 matrix in 1 row on D5
+MAX7219 <4 , 1, 5> mtrx; 
 
 void debug_output(const char* msg) {
   Serial.println(msg);
 }
 
-/**
- * Displays diff between some point in the past with the current time
-*/
-void non_stop_watch(DateTime diff) {
-
-}
-
-/**
- * Dispalays date in a specified format
-*/
-void display_handler(DateTime current_time) {
-  DateTime diff = current_time - target_date;
-  uint32_t seconds = diff.secondstime();
-
-  switch(DISPLAY_MODE) {
-    case dispalay_mode::DECIMAL: {
-      debug_output("go to display decimal");
-      display_decimal(seconds);    
-    }; break;
-    case dispalay_mode::BINARY: {}; break;
-    case dispalay_mode::OCTAL: {}; break;
-    case dispalay_mode::TEXT: {}; break;
-  }
-}
-
-
 void button_mode_handler() {
-  int8_t DISPLAY_MODE = dispalay_mode::DECIMAL;
+  // todo
 }
 
 /**
@@ -80,13 +33,14 @@ void button_mode_handler() {
  * - sends data to the display handler
 */
 void rtc_interruption_handler() {
-  debug_output("SQW interruption handler");
-  time = rtc.now();
-  display_handler(time);
+  seconds++;
+  Serial.println(seconds);
+  Serial.println(seconds, 2);
+  debug_output("SQW_INT");
 }
 
 void button_setup() {
-    // assign interruption handler
+  // assign interruption handler
   pinMode(MODE_BUTTON_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), rtc_interruption_handler, FALLING);
 }
@@ -101,16 +55,20 @@ void button_setup() {
 */
 void rtc_setup(){
   // connect via I2C to the ds3221
-  if (!rtc.begin()){
+  while (!rtc.begin()){
     debug_output("Couldn't connect to the ds3221");
     Serial.flush();
-    while (1) delay(10);
+    _delay_ms(10);
   }
+  
   // setup compile time if there were powered off
   if (rtc.lostPower()) {
     debug_output("RTC lost power, setup compile time");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  } else {
+    debug_output("RTC hasnt lost power");
   }
+  
   //we don't need the 32K Pin, so disable it
   rtc.disable32K();
 
@@ -128,18 +86,39 @@ void rtc_setup(){
   rtc.writeSqwPinMode(DS3231_SquareWave1Hz);
 }
 
-void dispay_setup() {
+void display_setup() {
   mtrx.begin();
-  delay(3000);
+  mtrx.clear();
+  mtrx.update();
 }
 
 void setup() {
+  debug_output("start setup");
   if (DEBUG) {
-    Serial.begin(57600);
+    Serial.begin(9800);
   }
 
+  debug_output("display setup");
+  display_setup();
+  debug_output("button setup");
+  button_setup();
+  debug_output("rtc setup");
   rtc_setup();
+  seconds = rtc.now().secondstime() + DELTA;
+}
+
+void print_datetime() {
+    char date[14] = "YYYY:hh:mm:ss";
+    rtc.now().toString(date);
+    Serial.println(date);
 }
 
 void loop() {
+  mtrx.clear();
+  mtrx.setCursor(0, 0);
+  // mtrx.print(seconds);
+  char date[6] = "hh:mm";
+  rtc.now().toString(date);
+  mtrx.print(date);
+  mtrx.update(); 
 }
