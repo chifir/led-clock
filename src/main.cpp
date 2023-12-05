c642d9d07a33f36710990f2b8dde99c60529aa48
4ifir <4ifirus@gmail.com>
2023-12-05 22:29:56 +0300

    misc changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 
 #define DEBUG true
 #define CLOCK_INTERRUPT_PIN 2
-#define BUTTON_INTERRUPT_PIN 6
+#define BUTTON_CHANGE_MODE_PIN 6
+#define BUTTON_CHANGE_UNIT_PIN 7
 
 const uint8_t DISPLAY_WIDTH = 63;
 const uint8_t HEIGHT = 3;
 const uint8_t WiDITH = 3;
-
 const uint8_t MODE[5] = {2, 8, 10, 16, 0};
 const uint8_t MODE_SIZE = 5;
+const uint8_t DEBOUNCE_DELAY_MS = 50;
 
 enum display_mode{
   bin = 2,
@@ -30,24 +31,43 @@ volatile bool trigger_display_update = true;
 
 uint8_t CURRENT_MODE_INDEX = 0;
 
+uint32_t offset = 0;
+
+bool final_button_state = LOW;
+bool current_button_state = LOW;
+bool previous_button_state = LOW;
+uint8_t debounce_time_ms = 0;
 
 // SDA on A4, SCL on A5, SQW on D4
 RTC_DS3231 rtc; 
 RTC_I2C rtc_i2c;
 
 // 8 matrix in 1 row on D5
-MAX7219 <8 , 1, 5> mtrx; 
+MAX7219 <12 , 1, 5> mtrx; 
 
 void debug_output(const char* msg) {
   Serial.println(msg);
 }
 
-void button_mode_handler() {
-  if (CURRENT_MODE_INDEX >= 0 && CURRENT_MODE_INDEX < MODE_SIZE - 1) {
-    CURRENT_MODE_INDEX++;
-  } else if (CURRENT_MODE_INDEX == MODE_SIZE - 1) {
-    CURRENT_MODE_INDEX = 0;
+void debug_matrix_output(char* msg, double delay){
+  mtrx.clear();
+  mtrx.setCursor(0, 0);
+  mtrx.print(msg);
+  mtrx.update();
+  _delay_ms(delay);
+}
+
+void graph_sin(uint8_t offset) {
+  uint8_t x = 0;
+  uint8_t y = 0;
+  mtrx.clear();
+  mtrx.setCursor(0, 0);
+  for(uint8_t i = 0 + offset; i <= 95 + offset; i++) {
+    y = 4 * (1 + sinf(25.7 * i));
+    mtrx.dot(x, y);
+    x++;
   }
+  mtrx.update();
 }
 
 void display_bin(uint32_t time) {
@@ -122,6 +142,14 @@ void display_update() {
   }
 }
 
+void display_format_mode_change() {
+  if (CURRENT_MODE_INDEX >= 0 && CURRENT_MODE_INDEX < MODE_SIZE - 1) {
+    CURRENT_MODE_INDEX++;
+  } else if (CURRENT_MODE_INDEX == MODE_SIZE - 1) {
+    CURRENT_MODE_INDEX = 0;
+  }
+}
+
 /**
  * SQW signal interruption handler
  * 
@@ -132,10 +160,9 @@ void rtc_interruption_handler() {
   trigger_display_update = true;
 }
 
-void button_setup() {
-  // assign interruption handler
-  pinMode(BUTTON_INTERRUPT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), rtc_interruption_handler, FALLING);
+void buttons_setup() {
+  pinMode(BUTTON_CHANGE_MODE_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_CHANGE_UNIT_PIN, INPUT_PULLUP);
 }
 
 /**
@@ -177,6 +204,8 @@ void rtc_setup(){
 
   // start oscilaating at SQW with 1Hz
   rtc.writeSqwPinMode(DS3231_SquareWave1Hz);
+
+  CURRENT_MODE_INDEX = 1;
 }
 
 void display_setup() {
@@ -184,7 +213,6 @@ void display_setup() {
   mtrx.clear();
   mtrx.update();
 }
-
 
 void test_binary() {
   int x = 0;
@@ -213,7 +241,7 @@ void setup() {
   debug_output("display setup");
   display_setup();
   debug_output("button setup");
-  button_setup();
+  buttons_setup();
   debug_output("rtc setup");
   rtc_setup();
   seconds = rtc.now().secondstime() + DELTA;
@@ -225,6 +253,23 @@ void print_datetime() {
     Serial.println(date);
 }
 
+bool button_mode_read(){
+  int current_button_state = digitalRead(BUTTON_CHANGE_MODE_PIN);
+  if (current_button_state != previous_button_state) {
+    debounce_time_ms = millis();
+  }
+  if ((millis() - debounce_time_ms) >= DEBOUNCE_DELAY_MS) 
+  {
+    final_button_state = digitalRead(BUTTON_CHANGE_MODE_PIN);
+  }
+  previous_button_state = current_button_state;
+  return final_button_state;
+}
+
+
 void loop() {
   display_update();
+  if (button_mode_read()) {
+    display_format_mode_change();
+  }
 }
